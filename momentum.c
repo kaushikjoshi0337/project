e1927e9d5323c1a586736432f5710dbf01d0f069
dharmi9397 <patelzerox2008@gemail.com>
2024-04-24 13:35:40 +0530

    c language

---
+++ momentum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>>
+
+main()
+{
+int a;
+
+printf("enter the value of a");
+scanf("%d",&a);
+(a % 2==0)? printf("this number is even"): printf("this number is odd");
+
+	
+}
---
+++ momentum2.c
@@ -0,0 +1,45 @@
+#include<stdio.h>>
+main()
+{
+int unit;
+
+
+printf("enter the unit of bill unit");
+scanf("%d",&unit);
+printf("unit of bill",unit);
+
+    if(unit <=50){
+	bill = unit * 0.50;
+    }
+	
+	else if (unit <=100);{
+	bill = unit * 0.75;
+	}
+
+    7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
